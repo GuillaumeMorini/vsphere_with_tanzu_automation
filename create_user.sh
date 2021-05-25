b5e74f757e5d4753f6b750f5c88718874bae3216
Guillaume Morini <gmorini@pivotal.io>
2021-05-25 18:00:14 +0200

    add check govc is configured

--- create_user.sh
+++ create_user.sh
@@ -8,6 +8,12 @@ if [ $? -ne 0 ]
     exit 1
 fi
 
+if [ -z "$GOVC_URL" ] || [ -z "$GOVC_USERNAME" ]  || [ -z "$GOVC_PASSWORD" ]  
+  then
+    >&2 echo "govc is not configured, this is a prerequisite for this script to work"
+    exit 1
+fi
+
 ROLE=TKG
 USER=tkg-user
 DOMAIN=vsphere.local
