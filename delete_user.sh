3a533de438fc36d23c6c370d9ab32630ba8f9556
Guillaume Morini <gmorini@pivotal.io>
2021-05-25 17:55:28 +0200

    add check govc is installed

--- delete_user.sh
+++ delete_user.sh
@@ -1,4 +1,12 @@
 # delete_user.sh
+#!/bin/bash
+
+GOVC=$(which govc)
+if [ $? -ne 0 ]
+  then
+    >&2 echo "govc is not installed, this is a prerequisite for this script to work"
+    exit 1
+fi
 
 ROLE=TKG
 USER=tkg-user
